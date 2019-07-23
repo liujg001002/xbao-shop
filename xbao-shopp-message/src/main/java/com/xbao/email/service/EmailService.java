acedbf28881e557988c3080612bae7628a0c001b
liujg001002 <__ll623680>
2019-07-23 17:49:28 +0800

    add send email

--- xbao-shopp-message/src/main/java/com/xbao/email/service/EmailService.java
+++ xbao-shopp-message/src/main/java/com/xbao/email/service/EmailService.java
@@ -1,6 +1,10 @@
 package com.xbao.email.service;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
@@ -12,7 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class EmailService implements MessageAdapter {
-   
+
+	@Value("${msg.subject}")
+	private String subject;
+	@Value("${msg.text}")
+	private String text;
+	@Value("${spring.mail.username}")
+	private String fromEmail;
+	@Autowired
+	private JavaMailSender javaMailSender;
 	@Override
 	public void sendMsg(JSONObject body) {
 		// 处理发送邮件
@@ -21,6 +33,20 @@ public class EmailService implements MessageAdapter {
 			return ;
 		}
 		log.info("消息服务平台发送邮件:{}",email);
+		//##########发送邮件开始############
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+		// 来自账号 自己发自己
+		simpleMailMessage.setFrom(fromEmail);
+		// 发送账号
+		simpleMailMessage.setTo(email);
+		// 标题
+		simpleMailMessage.setSubject(subject);
+		// 内容
+		simpleMailMessage.setText(text.replace("{}", email));
+		// 发送邮件
+		javaMailSender.send(simpleMailMessage);
+		//##########发送邮件结束############
+		log.info("消息服务平台发送邮件:{}to{}完成{}", fromEmail,email);
 	}
 
 }
